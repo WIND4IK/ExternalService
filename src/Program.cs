d65caa2c9586908029e488e576bdae0ca1743616
Denis Burunov <denis.burunov@hys-enterprise.com>
2023-08-29 14:00:20 +0300

    Updated config

--- src/Program.cs
+++ src/Program.cs
@@ -5,12 +5,13 @@ builder.Services.AddServiceModelMetadata();
 builder.Services.AddSingleton<IServiceBehavior, UseRequestHeadersForMetadataAddressBehavior>();
 
 var app = builder.Build();
+//app.UseHttpsRedirection();
 
 app.UseServiceModel(serviceBuilder =>
 {
     serviceBuilder.AddService<ExternalServiceImpl>();
-    serviceBuilder.AddServiceEndpoint<ExternalServiceImpl, DomainBellaNS.API.ExternalService.ExternalService>(new BasicHttpBinding(), "/ExternalService");
+    serviceBuilder.AddServiceEndpoint<ExternalServiceImpl, DomainBellaNS.API.ExternalService.ExternalService>(new WSHttpBinding { Security = {Mode = SecurityMode.Transport, Transport = { ClientCredentialType = HttpClientCredentialType.None}}}, "/ExternalService");
     var serviceMetadataBehavior = app.Services.GetRequiredService<ServiceMetadataBehavior>();
-    serviceMetadataBehavior.HttpsGetEnabled = false;
+    serviceMetadataBehavior.HttpsGetEnabled = true;
 });
 app.Run();
